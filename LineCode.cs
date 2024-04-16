3cb1462794b8780e35e2a1a98c03db89be1c055f
Torence7 <130224465+Torence7@users.noreply.github.com>
2024-04-15 23:13:07 -0400

    Update LineCode.cs

--- LineCode.cs
+++ LineCode.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class LineCode : MonoBehaviour
 {
-    LineRenderer lineRenderer;
-    public Transform origin;
-    public Transform destination;
+    LineRenderer lineRenderer; // Reference to the LineRenderer component
+    public Transform origin; // The starting point of the line
+    public Transform destination; // The ending point of the line
+
     // Start is called before the first frame update
     void Start()
     {
-      lineRenderer = GetComponent<LineRenderer>();
-      lineRenderer.startWidth = 0.05f;
-      lineRenderer.endWidth = 0.05f;
+        // Get the LineRenderer component attached to this GameObject
+        lineRenderer = GetComponent<LineRenderer>();
+
+        // Set the width of the line
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Set the starting position of the line to the position of the origin transform
         lineRenderer.SetPosition(0, origin.position);
+        
+        // Set the ending position of the line to the position of the destination transform
         lineRenderer.SetPosition(1, destination.position);
     }
 }
