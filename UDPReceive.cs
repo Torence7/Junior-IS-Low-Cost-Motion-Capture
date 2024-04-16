d4e8e6bd7d26a79cc68fb38c4b6cbf8a13b11e25
Torence7 <130224465+Torence7@users.noreply.github.com>
2024-04-15 22:48:33 -0400

    Update UDPReceive.cs

--- UDPReceive.cs
+++ UDPReceive.cs
@@ -7,46 +7,56 @@ using System.Threading;
 
 public class UDPReceive : MonoBehaviour
 {
-
+    // Thread and client variables
     Thread receiveThread;
     UdpClient client;
+
+    // Port number for UDP communication
     public int port = 2411;
+
+    // Boolean variables for controlling receiving and printing behavior
     public bool startRecieving = true;
     public bool printToConsole = false;
+
+    // Variable to store received data
     public string data;
 
-
+    // Start method called when the script is initialized
     public void Start()
     {
-
+        // Start a new thread for receiving data
         receiveThread = new Thread(
             new ThreadStart(ReceiveData));
         receiveThread.IsBackground = true;
         receiveThread.Start();
     }
 
-
-    // receive thread
+    // Method for receiving data on a separate thread
     private void ReceiveData()
     {
-
+        // Initialize a UDP client to listen on the specified port
         client = new UdpClient(port);
+
+        // Continuously listen for data while startRecieving is true
         while (startRecieving)
         {
-
             try
             {
+                // Receive data from any IP address
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
                 byte[] dataByte = client.Receive(ref anyIP);
+                // Convert received byte array to string using UTF-8 encoding
                 data = Encoding.UTF8.GetString(dataByte);
 
+                // Print received data to the console if printToConsole is true
                 if (printToConsole) { print(data); }
             }
             catch (Exception err)
             {
+                // Print any exceptions that occur during receiving
                 print(err.ToString());
             }
         }
     }
-
 }
+
